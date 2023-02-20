7a5c04b3b7edd75b604151e9cb257a052154a014
Fikayo Jetawo <jetawof@gmail.com>
2023-02-20 22:57:38 +0100

    Landing Page mobile view

--- pages/index.js
+++ pages/index.js
@@ -15,17 +15,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='flex'>
-        <div className='hidden lg:block w-1/2 h-full flex flex-col justify-center container mx-auto bg-[#192026]'>
+      <div className='flex lg:flex-row flex-col h-screen lg:h-full '>
+        <div className=' lg:w-1/2 lg:h-full h-1/2 flex flex-col justify-center container mx-auto bg-[#192026]'>
           <img
-            className='object-cover h-screen w-full opacity-70'
+            className='object-cover lg:h-screen w-full opacity-70'
             src="https://images.pexels.com/photos/7869678/pexels-photo-7869678.jpeg?auto=compress&cs=tinysrgb&w=2000"
           />
         </div>
 
-        <div className='w-full h-screen lg:w-1/2 flex flex-col justify-center text-center overflow-y-auto'>
-          <div className='absolute top-0 backdrop-blur-lg'>
-            <h1 className=' px-10 py-10 text-5xl font-semibold text-slate-700 font-mono'>Work <i className="text-green-500">It!</i></h1>
+        <div className='w-full lg:h-screen h-1/2 lg:w-1/2 flex flex-col justify-center text-center overflow-y-auto z-20 bg-white'>
+          <div className='absolute top-0'>
+            <h1 className=' px-10 py-10 text-5xl font-semibold text-white lg:text-slate-700 font-mono'>Work <i className="text-green-500">It!</i></h1>
           </div>
           <h1 className='text-5xl leading-[60px]'><b className='text-green-600 font-semibold'>Health</b> is wealth <br /> Put in more <b className=' font-semibold text-green-600'>Effort</b> </h1>
           <p className='text-lg lg:text-xl m-4 text-slate-600 font-serif'>Take care of your body, it is the only place we have to live</p>
